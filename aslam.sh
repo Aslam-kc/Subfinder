e0b7d433d16370a0fbcf4adc1b07dbb07b8fbdf4
Aslam-kc <mohammedazlamkc@gmail.com>
2024-07-22 09:11:06 +0530

    Update aslam.sh

--- aslam.sh
+++ aslam.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/bash
+
 echo ""
 echo "    
   _             _        _                 
@@ -6,24 +7,53 @@ echo "
  | |/ / __|   / _ \ / __| |/ _` | '_ ` _ \ 
  |   < (__   / ___ \\__ \ | (_| | | | | | |
  |_|\_\___| /_/   \_\___/_|\__,_|_| |_| |_|
-                                                                                                                                                                                                                                                
-echo "
+ "
+echo ""
+
 greetings(){
-echo "Welcome `tput setaf 1` $USER! `tput sgr0`. You are using `tput setaf 6` LIVE SUBDOMAINS FINDER `tput sgr0` coded by `tput setaf 3` Aslam  `tput sgr0`" 
-echo "Please make sure you have `tput setaf 4`'assetfinder'`tput sgr0` and `tput setaf 4`'httprobe'`tput sgr0` installed in your system"
+    echo "Welcome `tput setaf 1` $USER! `tput sgr0`. You are using `tput setaf 6` LIVE SUBDOMAINS FINDER `tput sgr0` coded by `tput setaf 3` Aslam  `tput sgr0`" 
+    echo "Please make sure you have `tput setaf 4`'assetfinder'`tput sgr0` and `tput setaf 4`'httprobe'`tput sgr0` installed in your system"
 }
 greetings
 echo ""
-read -p "Enter Your Domain :" test       
-assetfinder $test > subs
-cat subs | httprobe > live
-sort -u live > sorted
-cat sorted
-echo ""
-echo "
-░▀█▀░█░█░█▀█░█▀█░█░█░░░█░█░█▀█░█░█░░░█▀▀░█▀█░█▀▄░░░█░█░▀█▀░█▀▀░▀█▀░▀█▀░░░█▀█░█░█░█▀▄░░░█▀█░█▀█░█▀▀░█▀▀
+
+spinner() {
+    local pid=$!
+    local delay=0.1
+    local spinstr='.....'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+while true; do
+    read -p "Enter Your Domain: " test
+    
+    echo -n "Finding subdomains..."
+    assetfinder $test > subs &
+    spinner
+    
+    echo -n "Checking live subdomains..."
+    cat subs | httprobe > live &
+    spinner
+    
+    sort -u live > sorted
+    cat sorted
+    
+    read -p "$(echo -e "\e[33mDo you want to continue with another domain? (yes/no):\e[0m ")" continue
+
+    if [ "$continue" != "yes" ]; then
+        echo -e "\e[31m
+░█▀▀░█░█░█▀█░█▀█░█░█░░░█░█░█▀█░█░█░░░█▀▀░█▀█░█▀▄░░░█░█░▀█▀░█▀▀░▀█▀░▀█▀░░░█▀█░█░█░█▀▄░░░█▀█░█▀█░█▀▀░█▀▀
 ░░█░░█▀█░█▀█░█░█░█▀▄░░░░█░░█░█░█░█░░░█▀▀░█░█░█▀▄░░░▀▄▀░░█░░▀▀█░░█░░░█░░░░█░█░█░█░█▀▄░░░█▀▀░█▀█░█░█░█▀▀
 ░░▀░░▀░▀░▀░▀░▀░▀░▀░▀░░░░▀░░▀▀▀░▀▀▀░░░▀░░░▀▀▀░▀░▀░░░░▀░░▀▀▀░▀▀▀░▀▀▀░░▀░░░░▀▀▀░▀▀▀░▀░▀░░░▀░░░▀░▀░▀▀▀░▀▀▀
-"
-
-
+\e[0m"
+        echo ""
+        break
+    fi
+done
